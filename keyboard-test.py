bddc566364835490883c539bd8236a22ecdf7399
mundoembarcado <76628246+mundoembarcado@users.noreply.github.com>
2021-09-15 09:08:57 -0300

    Add files via upload

---
+++ keyboard-test.py
@@ -0,0 +1,9 @@
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+# Press and release space
+keyboard.type('Hello World acentuação')
+
+#code
+https://www.codegrepper.com/code-examples/python/py+keyboard
